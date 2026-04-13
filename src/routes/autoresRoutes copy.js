1bd907cb994d8003b510335e15b83ad81c5d04be
Hélio Levi M. V. <hlevi200730@gmail.com>
2026-04-13 14:38:53 -0300

    Subir commit antes de atualização com ia de substituição da palavra livros por autor.

---
+++ src/routes/autoresRoutes copy.js
@@ -0,0 +1,12 @@
+import express from "express";
+import LivroController from "../controller/livroController.js";
+
+const  routes = express.Router();
+
+routes.get("/livros", LivroController.listarLivros);
+routes.post("/livros", LivroController.cadastrarLivro);
+routes.get("/livros/:id", LivroController.listarLivrPorId);
+routes.put("/livros/:id", LivroController.atualizarLivro);
+routes.delete("/livros/:id", LivroController.excluirLivro);
+
+export default routes;
